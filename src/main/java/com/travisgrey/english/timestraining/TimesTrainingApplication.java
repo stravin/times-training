25e2ee0bc9b538fe5c6cabd072966c29081d9e36
Sergey Travin <sergey.a.travin@gmail.com>
2017-07-23 22:06:33 +0600

    Initial commit

---
+++ src/main/java/com/travisgrey/english/timestraining/TimesTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.travisgrey.english.timestraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesTrainingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/travisgrey/english/timestraining/TimesTrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.travisgrey.english.timestraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimesTrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
